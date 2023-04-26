f7ac12cc6f33a43566609dab57ba480f184aa937
chchan18 <122188225+chchan18@users.noreply.github.com>
2023-04-26 14:43:59 +0800

    Update learningnew.py

--- learningnew/learningnew.py
+++ learningnew/learningnew.py
@@ -1,9 +1,9 @@
-import random
+'''import random
 random.randint(16,272)
 
-1%5
-2**5
-import math
+print(1%5)
+print(2**5)
+import math''
 
 
 hi="hello there"
@@ -11,5 +11,10 @@ name="ana"
 greet=hi+" " +name
 silly=hi+(" "+name)*3
 x=1
-x_str=str(x)
+x_str=str(x)'''
+
+
+import emoji
+str_emoji="This is emoji :eight_pointed_black_star:"
+print(emoji.emojize(str_emoji,language='alias',variant="emoji_type"))
 
